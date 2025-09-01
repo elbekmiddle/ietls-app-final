67ebf67dc33ec4faff2439c915b51756b00e7df9
elbek <elbek4561@gmail.com>
2025-09-01 11:33:21 +0500

    mongodb

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,8 +1,7 @@
-
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const databaseConfig = {
-  uri: process.env.MONGO_URI || 'mongodb+srv://asadillohdeveloper05_db_user:Asadilloh2004@cluster0.yewbxk1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test',
+  uri: process.env.MONGO_URI || 'mongodb://localhost/ielts-project',
 };
